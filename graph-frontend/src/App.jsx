01ed6a3504ca4df375caff48db7d88dce2073bc5
DragosTrinca <dragostrinca0@gmail.com>
2026-07-12 00:56:06 +0300

    Added graph input section

--- graph-frontend/src/App.jsx
+++ graph-frontend/src/App.jsx
@@ -6,40 +6,75 @@ function App() {
     const canvasRef = useRef(null);
 
     const [status, setStatus] = useState("Waiting...");
+    const [inputSource, setInputSource] = useState("");
+    const [inputTarget, setInputTarget] = useState("");
+    const [inputStartNode, setInputStartNode] = useState("0");
+    const [uiEdges, setUiEdges] = useState([]);
 
+    const nodesRef = useRef([]);
+    const edgesRef = useRef([]);
     const stepsRef = useRef([]);
     const stepIndexRef = useRef(-1);
 
-    const initialNodes = [
-        { id: '0', x: Math.random() * 800, y: Math.random() * 600, vx: 0, vy: 0 },
-        { id: '1', x: Math.random() * 800, y: Math.random() * 600, vx: 0, vy: 0 },
-        { id: '2', x: Math.random() * 800, y: Math.random() * 600, vx: 0, vy: 0 },
-        { id: '3', x: Math.random() * 800, y: Math.random() * 600, vx: 0, vy: 0 },
-        { id: '4', x: Math.random() * 800, y: Math.random() * 600, vx: 0, vy: 0 }
-    ];
+    const handleAddEdge = (e) => {
+        e.preventDefault();
 
-    const initialEdges = [
-        { source: '0', target: '1' },
-      	{ source: '0', target: '2' },
-      	{ source: '1', target: '3' },
-      	{ source: '2', target: '4' },
-      	{ source: '3', target: '4' }
-    ];
+        if (inputSource === "" || inputTarget === "") return;
+
+        const addNodeIfMissing = (nodeId) => {
+            const exists = nodesRef.current.find(n => n.id === nodeId);
+            if (!exists) {
+                nodesRef.current.push({
+                    id: nodeId,
+                    x: Math.random() * 800,
+                    y: Math.random() * 600,
+                    vx: 0,
+                    vy: 0
+                });
+            }
+        };
+
+        addNodeIfMissing(inputSource);
+        addNodeIfMissing(inputTarget);
+
+        const newEdge = {
+            source: inputSource,
+            target: inputTarget
+        };
+        edgesRef.current.push(newEdge);
+
+        setUiEdges([...edgesRef.current]);
+        setInputSource("");
+        setInputTarget("");
+    };
 
     const fetchBFS = async () => {
+        if (nodesRef.current.length === 0) {
+            alert("Please add at least 1 node");
+            return;
+        }
+
         setStatus("Processing...");
+
+        const adjList = {};
+        edgesRef.current.forEach(edge => {
+            // Initialize arrays
+            if (!adjList[edge.source]) adjList[edge.source] = [];
+            if (!adjList[edge.target]) adjList[edge.target] = [];
+
+            if (!adjList[edge.source].includes(parseInt(edge.target))) {
+                adjList[edge.source].push(parseInt(edge.target));
+            }
+            if (!adjList[edge.target].includes(parseInt(edge.target))) {
+                adjList[edge.source].push(parseInt(edge.target));
+            }
+        });
 
         // Convert edges into an adjacency list
         const payload = {
             algorithm: "BFS",
-            startNode: 0,
-            adjacencyList: {
-                "0": [1, 2],
-                "1": [0, 3],
-                "2": [0, 4],
-                "3": [1, 4],
-                "4": [2, 3]
-            }
+            startNode: parseInt(inputStartNode),
+            adjacencyList: adjList
         };
 
         try {
@@ -70,7 +105,7 @@ function App() {
         canvas.height = 600;
 
         const renderer = new Renderer(canvas);
-		const physics = new PhysicsEngine(initialNodes, initialEdges, canvas.width, canvas.height);
+		const physics = new PhysicsEngine(nodesRef.current, edgesRef.current, canvas.width, canvas.height);
 
 		let animationFrameId;
 
@@ -82,9 +117,9 @@ function App() {
 			renderer.clearScreen();
 
 			// Draw edges
-			initialEdges.forEach(edge => {
-				const n1 = initialNodes.find(n => n.id === edge.source);
-				const n2 = initialNodes.find(n => n.id === edge.target);
+			edgesRef.current.forEach(edge => {
+				const n1 = nodesRef.current.find(n => n.id === edge.source);
+				const n2 = nodesRef.current.find(n => n.id === edge.target);
 				if (n1 && n2)
 					renderer.drawLine(n1.x, n1.y, n2.x, n2.y);
 			});
@@ -95,7 +130,7 @@ function App() {
             const currentStepData = currentIndex >= 0 ? currentStepsArray[currentIndex] : null; 
 
 			// Draw nodes
-			initialNodes.forEach(node => {
+			nodesRef.current.forEach(node => {
                 let color = '#3333f9';
 
                 if (currentStepData) {
@@ -130,25 +165,73 @@ function App() {
     }
 
     return (
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '20px'}}>
-            <h2>Graph Visualizer</h2>
+        <div className="app-container">
+            <div className = "sidebar">
+                <h3>Graph Editor</h3>
 
-            <div style={{ marginBottom: '15px', gap: '10px', display: 'flex', alignItems: 'center'}}>
-                <button onClick={fetchBFS} style={{ padding: '8px 16px', cursor: 'pointer'}}>BFS</button>
-                <button onClick={prevStep} style={{ padding: '8px 16px', cursor: 'pointer'}}>Previous</button>
-                <button onClick={nextStep} style={{ padding: '8px 16px', cursor: 'pointer'}}>Next</button>
-                <span style={{ color: '#555'}}>Status: {status}</span>
+                <form onSubmit={handleAddEdge} className="edge-form">
+                    <div className="input-group">
+                        <label>Source Node:</label>
+                        <input
+                            type="text"
+                            className="input-field"
+                            value={inputSource}
+                            onChange={e => setInputSource(e.target.value)}
+                            placeholder="Ex. 0"
+                        />
+                    </div>
+
+                    <div className="input-group">
+                        <label>Destination Node:</label>
+                        <input
+                            type="text"
+                            className="input-field"
+                            value={inputTarget}
+                            onChange={e => setInputTarget(e.target.value)}
+                            placeholder="Ex. 1"
+                        />
+                    </div>
+                    <button type="submit" className="btn btn-add">Add Edge</button>
+                </form>
+
+                <h4>Current Edges:</h4>
+                <ul className="edges-list">
+                    {uiEdges.length === 0 ? <li>No edge</li> : uiEdges.map((e, i) => (
+                        <li key={i}>{e.source} - {e.target}</li>
+                    ))}
+                </ul>
+
+                <hr className="separator"/>
+
+                <h3>BFS Controls</h3>
+                <div className="bfs-controls">
+                    <div className="input-group">
+                        <label>Start Node:</label>
+                        <input
+                            type="text"
+                            className="input-field"
+                            value={inputStartNode}
+                            onChange={e => setInputStartNode(e.target.value)}
+                            placeholder="Ex. 0"
+                        />
+                    </div>
+                    <button onClick={fetchBFS} className="btn btn-submit">Submit</button>
+                </div>
+
+                <div className="step-controls">
+                    <button onClick={prevStep} className="btn btn-step">Previous</button>
+                    <button onClick={nextStep} className="btn btn-step">Next</button>
+                </div>
+
+                <p className="status-text">Status: {status}</p>
             </div>
 
-            <canvas
-                ref={canvasRef}
-                style={{
-                    backgroundColor: 'f0f0f0',
-                    border: '2px solid #ccc',
-                    borderRadius: '8px'
-                }}
-            />
+
+            <div className="canvas-container">
+                <canvas ref={canvasRef} className="graph-canvas"/>
+            </div>
         </div>
+        
     );
 }
 
