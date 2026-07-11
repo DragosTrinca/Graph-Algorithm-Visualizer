33a67ba5e51d1da6b17310e1cfb045d0304bade5
DragosTrinca <dragostrinca0@gmail.com>
2026-07-11 16:07:23 +0300

    Added graph animation

--- graph-frontend/src/App.jsx
+++ graph-frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useRef, useEffect } from 'react';
 import { Renderer } from './Renderer';
+import { PhysicsEngine} from './PhysicsEngine';
 
 function App() {
     const canvasRef = useRef(null);
@@ -12,13 +13,52 @@ function App() {
 
         const renderer = new Renderer(canvas);
 
-        renderer.clearScreen();
+        const initialNodes = [
+            { id: '0', x: Math.random() * 800, y: Math.random() * 600, vx: 0, vy: 0 },
+            { id: '1', x: Math.random() * 800, y: Math.random() * 600, vx: 0, vy: 0 },
+            { id: '2', x: Math.random() * 800, y: Math.random() * 600, vx: 0, vy: 0 },
+            { id: '3', x: Math.random() * 800, y: Math.random() * 600, vx: 0, vy: 0 },
+            { id: '4', x: Math.random() * 800, y: Math.random() * 600, vx: 0, vy: 0 }
+        ];
 
-        renderer.drawLine(200, 200, 500, 400);
+        const initialEdges = [
+            { source: '0', target: '1' },
+      		{ source: '0', target: '2' },
+      		{ source: '1', target: '3' },
+      		{ source: '2', target: '4' },
+      		{ source: '3', target: '4' }
+    	];
 
-        renderer.drawNode(200, 200, '#3498db', '0');
-        renderer.drawNode(500, 400, '#e74c3c', '1');
+		const physics = new PhysicsEngine(initialNodes, initialEdges, canvas.width, canvas.height);
 
+		let animationFrameId;
+
+		const animate = () => {
+			// Calculate new positions
+			physics.update();
+
+			// Clear the previous screen
+			renderer.clearScreen();
+
+			// Draw edges
+			initialEdges.forEach(edge => {
+				const n1 = initialNodes.find(n => n.id === edge.source);
+				const n2 = initialNodes.find(n => n.id === edge.target);
+				if (n1 && n2)
+					renderer.drawLine(n1.x, n1.y, n2.x, n2.y);
+			});
+
+			// Draw nodes
+			initialNodes.forEach(node => {
+				renderer.drawNode(node.x, node.y, '#224499', node.id);
+			});
+
+			animationFrameId = requestAnimationFrame(animate);
+		};
+
+		animate();
+
+		return () => cancelAnimationFrame(animationFrameId);
     }, []);
 
     return (
